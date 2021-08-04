1fc56334b510f46de5191a3492987288a5f46516
SettingDust <settingdust@gmail.com>
2021-08-04 11:30:14 +0800

    build(gradle): Add maven plugin
    Signed-off-by: SettingDust <settingdust@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.5.21"
     id("org.jlleitschuh.gradle.ktlint") version "10.1.0"
-    `maven-publish`
+    maven
 }
 
 group = "io.github.settingdust.bilive"
